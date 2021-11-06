55c8918447fb186388b3734b9a6ec1668523161e
DzPian <490857007@qq.com>
2021-11-06 15:11:53 +0800

    捷达有礼CK

---
+++ jdylCOOKIE.js
@@ -0,0 +1,80 @@
+﻿module.exports = {
+    "id": "jdyl",
+    "name": "捷达有礼",
+    "keys": ["jdylheader","jdylheader2","jdylheader3","jdylheader4","jdylheader5"],
+    "author": "@tom",
+    "settings": [{
+      "id": "jdylSuffix",
+      "name": "当前账号",
+      "val": "1",
+      "type": "number",
+      "desc": "当前抓取ck记录的账号序号，如：1、2、3、4"
+    }, {
+      "id": "jdylCount",
+      "name": "账号个数",
+      "val": "1",
+      "type": "number",
+      "desc": "指定任务最多跑几个账号，根据抓取的账号数据个数来设值"
+    }, {
+      "id": "jdylXH",
+      "name": "循环获取ck",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "jdylTXTX",
+      "name": "txtx",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "jdylSC",
+      "name": "sc",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "jdylnotifyttt",
+      "name": "推送控制",
+      "val": "1",
+      "type": "number",
+      "desc": "0关闭，1推送,默认12点以及23点推送"
+    }, {
+      "id": "jdylnotifyInterval",
+      "name": "通知控制",
+      "val": "2",
+      "type": "number",
+      "desc": "0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知"
+    }, {
+      "id": "jdylMinutes",
+      "name": "推送-通知 分钟控制",
+      "val": "10",
+      "type": "number",
+      "desc": "推送以及通知控制在什么分钟段，可设置0-59,默认0到10"
+    }],
+    "repo": "https://gitee.com/tom210120/tom/raw/master/JDYL/jdyl.js",
+    "icons": ["https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg", "https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg"],
+    "script": "https://gitee.com/tom210120/tom/raw/master/JDYL/jdyl.js",
+    "icon": "https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg",
+    "favIcon": "mdi-star-outline",
+    "favIconColor": "grey",
+    "datas": [{
+      "key": "jdylheader",
+      "val": "" //这里填账号1的token，其他地方不要改
+    }, {
+      "key": "jdylheader2", 
+      "val": "" //这里填账号2的token
+    }, {
+      "key": "jdylheader3",
+      "val": "" //这里填账号3的token
+    }, {
+      "key": "jdylheader4",
+      "val": "" //这里填账号4的token
+    }, {
+      "key": "jdylheader5",
+      "val": "" //这里填账号5的token
+    }],
+    "sessions": [],
+    "isLoaded": true
+  }
+  
