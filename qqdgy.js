ec2b18260d05df456827856ba2635499cf917b74
DzPian <490857007@qq.com>
2021-12-03 10:34:37 +0800

    琪琪的果园

--- qqdgy.js
+++ qqdgy.js
@@ -1,6 +1,6 @@
 // 脚本： 琪琪的果园
 // 说明：只支持青龙，需要手动抓包，打开小程序后抓包，www.weiju123.com 的任意post请求的url 中Cookie，和请求数据body中的 aot 参数
-// export qqck='PHPSESSID=9oeq2l7njitqbvt..........@PHPSESSID=9oeq2l7njitqbvt..........'
+// export qqck='PHPSESSID=9oeq2l7njitqbvt..........@PHPSESSID=9oeq2l7njitqbvt.........'
 //  export qqaot='.......@........'
 //❗️❗️❗️❗️ 脚本挂机后不要再小程序重新授权登录，会导致登录失效，就需要更新ck,❗️❗️❗️❗️
 // 11.28更新提现问题
