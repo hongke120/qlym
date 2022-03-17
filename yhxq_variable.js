e75ed31e4595d4f618b5dd7e226c5a4737cf425d
DzPian <490857007@qq.com>
2022-03-17 18:17:44 +0800

    源火星球配置文件

---
+++ yhxq_variable.js
@@ -0,0 +1,15 @@
+module.exports = {"code":200,"yhxq_variable_data":{
+    "config":[{
+        "url":"https://gitee.com/soy-tool/app-script/raw/master/app_yhxq.js",
+        "update":false,
+        "notice":false(推送通知,true代表推送,false反之)
+    }],
+    "user_data":[{
+        "token":"请求头上的Authorization值",
+        "Withdrawal":false,
+        "Withdrawal_Time":8,
+        "Feed":false(喂养,true代表喂养,false反之),
+        "UA":"请求UA头,请求头的User-Agent值(选填)"
+    }
+    
+]}}
