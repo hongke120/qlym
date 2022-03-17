d004d02beb0aab3e005f98e650fe1a36110ab97f
DzPian <490857007@qq.com>
2022-03-18 07:51:50 +0800

    源火星球依赖文件

---
+++ raw_master_yhxq_variable.js
@@ -0,0 +1,15 @@
+module.exports = {"code":200,"yhxq_variable_data":{
+    "config":[{
+        "url":"https://gitee.com/soy-tool/app-script/raw/master/app_yhxq.js",
+        "update":false,
+        "notice":false
+    }],
+    "user_data":[{
+        "token":"",
+        "Withdrawal":false,
+        "Withdrawal_Time":8,
+        "Feed":false,
+        "UA":""
+    }
+    
+]}}
