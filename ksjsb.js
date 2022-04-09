8512cbee8c5652c1d73bb81c2c7a2622575f6a64
weizuer <851474587@qq.com>
2022-04-10 01:46:37 +0800

    快手极速版1.08

--- ksjsb.js
+++ ksjsb.js
@@ -5,9 +5,10 @@ IOS/安卓： 快手极速版
 金币每天0点自动兑换到现金余额
 
 V2P和圈X配置好重写后，应该打开APP就能获取到CK，获取不到的话升级下app或者手动捉包
-青龙把任意包里的kuaishou.api_st=xxxxxxxxxxxx;这一串东西放到变量ksjsbCookie里，多账户换行或者用@隔开
+变量：ksjsbCookie 多账户换行或者用@隔开
+
 export ksjsbCookie='kuaishou.api_st=xxxxxxxxxxxx;
-kuaishou.api_st=yyyyyyyyy;'
+
 
 默认每天15点提现，要改的话把提现时间填到变量ksjsbWithdrawTime里
 默认按照账户的提现列表从高到低提现到绑定的提现账号，都有绑定的话默认提现到支付宝。要固定提现金额的话填到变量ksjsbCash里。如果提现失败，手动接验证码提现一次
@@ -15,14 +16,6 @@ kuaishou.api_st=yyyyyyyyy;'
 
 定时一天最少10次就行，最好改掉默认时间，不然太多人同一时间跑
 
-重写：
-[task_local]
-#快手极速版
-38 9-19 * * * https://raw.githubusercontent.com/leafxcy/JavaScript/main/ksjsb.js, tag=快手极速版, enabled=true
-[rewrite_local]
-appsupport/yoda/biz/info url script-request-header https://raw.githubusercontent.com/leafxcy/JavaScript/main/ksjsb.js
-ksapp/client/package/renew url script-request-header https://raw.githubusercontent.com/leafxcy/JavaScript/main/ksjsb.js
-[MITM]
 #IOS用第一个，安卓用第二个
 hostname = api.kuaisho*.com
 hostname = open.kuaisho*.com
